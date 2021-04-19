8ead7c5d47311a53fe23af404e63e52a046a6873
valfortdi <82848536+valfortdi@users.noreply.github.com>
2021-04-20 02:19:55 +0300

    Add files via upload

---
+++ src/main/java/Calc/Calculator.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        int result = calc(num1,num2,operation);
+        System.out.println("Result: "+result);
+    }
+
+    public static int getInt(){
+        System.out.println("Please insert number here:");
+        int num;
+        if(scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("You make a mistake, please try again");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation(){
+        System.out.println("Choose operation: +, -, *, /,");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("You make a mistake, please try again");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    public static int calc(int num1, int num2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Operation is invalid, please try again");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
+}
